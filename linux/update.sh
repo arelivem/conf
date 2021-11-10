f94901a9a3cda4fe3a17c4c3afd6539e65df4777
arelivem <arelive@yeah.net>
2021-11-11 01:44:13 +0800

    updata shell

---
+++ linux/update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# update bash-completion
+cd ~/.local/bash-completion/bash_completion && git pull origin master && cd -
+
+# update git-completion
+cd ~/.local/git-completion && /bin/rm git-completion.bash && wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash && cd -
